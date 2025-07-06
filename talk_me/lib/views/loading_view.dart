cd3cb7cab6adcfe010d5c579404934d1ca428ca7
Cesaarrrr <cabreracesar644@gmail.com>
2025-07-06 14:55:51 -0500

    vista loading lista
    vista loading lista basica

--- talk_me/lib/views/loading_view.dart
+++ talk_me/lib/views/loading_view.dart
@@ -1,1 +1,20 @@
-//aqui va la carga
+import 'package:flutter/material.dart';
+
+class loading extends StatelessWidget {
+  const loading({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          CircularProgressIndicator(),
+          Text('cargando...')
+        ],
+      ),
+    );
+  }
+}
+

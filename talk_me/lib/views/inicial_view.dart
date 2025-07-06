4ca92cc3acd68d1bbd23655d4fe99cd0b6b4e6ab
Cesaarrrr <cabreracesar644@gmail.com>
2025-07-06 14:47:29 -0500

    vista inicial
    vista inicial basica

--- talk_me/lib/views/inicial_view.dart
+++ talk_me/lib/views/inicial_view.dart
@@ -1,1 +1,18 @@
-//aqui va el bienvenido
+import 'package:flutter/material.dart';
+
+class inicio extends StatelessWidget {
+  const inicio({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('hola')
+        ],
+      )
+    );
+  }
+}

8be080d592fcd556f46a6f51b4ca9d189d723774
Cesaarrrr <cabreracesar644@gmail.com>
2025-07-06 15:58:30 -0500

    vista inicio
    vista inicio lista faltante

--- talk_me/lib/views/inicial_view.dart
+++ talk_me/lib/views/inicial_view.dart
@@ -8,11 +8,35 @@ class inicio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text('hola')
-        ],
-      )
+    backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 8),
+              Text(
+                'inicie su chat ',
+                style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 24),
+              ElevatedButton(
+              onPressed: () {
+                //accion bloc
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+              ),
+              child: Text('Iniciar chat'),
+            ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }

6866012f20c98013eda41b0edf2949593fcccc0b
Cesar <cabreracesar644@gmail.com>
2025-07-07 10:47:51 -0500

    loading actualizada

--- talk_me/lib/main.dart
+++ talk_me/lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Talk Me!',
       home: BlocProvider(
         create: (context) => ChatBloc(OpenAIService()),
--- talk_me/lib/views/loading_view.dart
+++ talk_me/lib/views/loading_view.dart
@@ -1,20 +1,62 @@
 import 'package:flutter/material.dart';
 
-class Loading extends StatelessWidget {
-  const Loading({
-    super.key,
-  });
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..repeat(reverse: true);
+
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 1.1).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          CircularProgressIndicator(),
-          Text('cargando...')
-        ],
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ScaleTransition(
+              scale: _scaleAnimation,
+              child: Image.asset(
+                'assets/logo.png',
+                width: 100,
+              ),
+            ),
+            const SizedBox(height: 24),
+            const Text(
+              'Cargando Chat...',
+              style: TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontSize: 18,
+              ),
+            ),
+           
+          ],
+        ),
       ),
     );
   }
 }
-
